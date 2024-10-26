da7dbd19230ebeb8ea59b7f095aca82ff84175bc
Ryan Cepeda <ryancepeda2@gmail.com>
2024-10-26 14:12:12 -0400

    add fxml file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.cs213project3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.sql;
 
 
     opens com.example.cs213project3 to javafx.fxml;
